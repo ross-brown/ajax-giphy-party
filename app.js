03ba772d6c8e798fa238946d5994d66383fb1c5c
ross-brown <rbrown_98@yahoo.com>
2023-08-15 18:19:34 -0500

    add form and functionality to append gif to DOM

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Let's get this party started!");
+"use strict";
+
+
+function showGif(url) {
+  const $newGif = $("<img>");
+  $newGif.attr("src", url);
+
+
+  $(".gifs").append($newGif);
+}
+
+
+
+
+
+async function handleSubmit(e) {
+  e.preventDefault();
+
+  const searchValue = $("#search-input").val();
+  const params = new URLSearchParams({ q: searchValue, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" });
+
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?${params}`);
+  const data = await response.json();
+
+  const randomNumber = Math.floor(Math.random() * 50);
+
+  const gifUrl = data.data[randomNumber].images.original.url;
+
+  showGif(gifUrl);
+}
+
+
+$("#submit-btn").on("click", handleSubmit);
